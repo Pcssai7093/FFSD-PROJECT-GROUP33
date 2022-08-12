60c676cb94b7cb990e6c3dd45f651092c34c1b82
raja <pcssai7093@gmail.com>
2022-08-12 17:50:59 +0530

    commited

--- transactions/transactionmodel.js
+++ transactions/transactionmodel.js
@@ -1,33 +1,36 @@
-const mongoose=module.require('mongoose');
-const schm=mongoose.Schema;
+const mongoose = module.require("mongoose");
+const schm = mongoose.Schema;
 
-const transschm=new schm({
-    
-    userid:{
-        type:String,
-        required:true
+const transschm = new schm(
+  {
+    userid: {
+      type: String,
+      required: true,
     },
-    gigid:{
-        type:String,
-        required:true
+    gigid: {
+      type: String,
+      required: true,
     },
-    amount:{
-        type:Number,
-        required:true
+    amount: {
+      type: Number,
+      required: true,
     },
-    reports:{
-        type:[{
-            type:String,
-        }]
+    reports: {
+      type: [
+        {
+          type: String,
+        },
+      ],
     },
-    review:{
-        type:String
+    review: {
+      type: String,
     },
-    rating:{
-        type:Number
-    }
-},{timestamps:true},);
+    rating: {
+      type: Number,
+    },
+  },
+  { timestamps: true }
+);
 
-
-const transdatamodel=mongoose.model('trans-datas',transschm);
-module.exports=transdatamodel;
+const transdatamodel = mongoose.model("trans-datas", transschm);
+module.exports = transdatamodel;
